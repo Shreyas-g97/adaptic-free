6a48aefebfb601cd9af48c8754bbfc3b7aea67d2
Shreyas Garimella <shreyas@adaptichealth.io>
2023-11-17 16:05:51 -0500

    google analytics + auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=G-5XKKLQN1Z9"></Script>
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-5XKKLQN1Z9');
+          `,
+        }}
+      />
+      <Script
             strategy="afterInteractive"
             dangerouslySetInnerHTML={{
             __html: `
